d333d3e3997c4c2f5486e515aa5ac05d5538a38e
GorkaTsybin <egortsybin@yandex.ru>
2024-08-02 14:55:00 +0400

    Add files via upload

---
+++ 1st program.py
@@ -0,0 +1,16 @@
+#"1st program"
+print(9 ** 0.5 * 5)
+#"2nd program"
+print(9.99 > 9.98 and 1000 !=1000.1)
+#"3rd program"
+num = 1234
+mid1 = str(num)[1:3]
+num = 5678
+mid2 = str(num) [1:3]
+print(int(mid1) + int(mid2))
+#"4th program"
+num1 = int(round(42.13 % 1, 2) *100)
+num2 = int(13.42)
+print(num1 == num2)
+
+
